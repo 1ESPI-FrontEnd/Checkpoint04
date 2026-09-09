fc096b6c511b33b1f4efd459fbb8515979787857
Felipe Rabelo <feliperabelo2010@gmail.com>
2026-09-09 14:00:20 -0300

    [Style] Estilizando botão de status

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -71,21 +71,21 @@ const Tarefas = () => {
                 Ela garante que RemoverTarefa só seja executada quando o evento acontecer (como um clique de botão), 
                 e não assim que a página carregar.
                 */}
-                <div className="">
+                <div className="flex items-center gap-2">
 
-                    <label htmlFor="concluida" className="">
-                        {tarefa.status}
-                        <button onClick={()=> TarefaConcluida(tarefa.id)}
-                            className="bg-gray-700 hover:bg-gray-800 font-medium px-5 py-1 rounded-2xl transition-colors cursor-pointer">
-                            {tarefa.status === "Pendente" ? "🟪" : "☑️"}
-                        
-                        </button>
-                    </label>
-                    <button onClick={()=> RemoverTarefa(tarefa.id)}
-                    className="bg-gray-700 hover:bg-gray-800 font-medium px-5 py-1 rounded-2xl transition-colors cursor-pointer">
-                    X
+                <label htmlFor="concluida" className="flex items-center gap-2 font-medium">
+                    {tarefa.status}
+                    <button onClick={()=> TarefaConcluida(tarefa.id)}
+                        className="bg-gray-700 hover:bg-gray-800 font-medium px-5 py-1 rounded-2xl transition-colors cursor-pointer">
+                        {tarefa.status === "Pendente" ? "🟪" : "☑️"}
+                     
                     </button>
-                    </div>
+                </label>
+                <button onClick={()=> RemoverTarefa(tarefa.id)}
+                className="bg-gray-700 hover:bg-gray-800 font-medium px-5 py-1 rounded-2xl transition-colors cursor-pointer">
+                X
+                </button>
+                </div>
             </li>
             ))}
         </ul>
